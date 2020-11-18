2f14a117a6bf4027a18ab873a53ffe437973b3e0
WinsonTay <53472821+WinsonTay@users.noreply.github.com>
2020-11-18 17:29:53 +0800

    added authentication and authorization, added Social Login, new Rental

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(RentalDto NewRentaldto)
+        {
+            var customer = db.Customers.SingleOrDefault(c => c.id == NewRentaldto.CustomerId);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            var movies = db.Movies.Where(m => NewRentaldto.MoviesId.Contains(m.id)).ToList();
+            var newRentals = new List<Rental>();
+
+            foreach (var movie in movies)
+            {
+                var movieAvailability = db.Movies.SingleOrDefault(m => m.id == movie.id);
+                if(movieAvailability.NumberAvailable != 0)
+                {
+                   movieAvailability.NumberAvailable = movieAvailability.NumberAvailable - 1;
+
+                    newRentals.Add(new Rental
+                    {
+                        Customer = customer,
+                        Movie = movie,
+                        DateRented = DateTime.Now
+                    });
+
+
+                }
+                db.SaveChanges();
+            }
+          
+            
+        
+            return Ok(new { message = "success"});
+        }
+
+    }
+}
---
+++ Vidly/Migrations/202011171127146_SeedUsers.cs
@@ -0,0 +1,27 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'a80b66dd-4248-46fe-a3c9-c9504b3f03c0', N'admin@vidly.com', 0, N'AC4sL2M9f57CNgUbPlUZqWvkcFpnTBKdmRNWlQtibpTX29mddXLC92N5qwJzUbid9w==', N'f1a72919-7047-49cf-b863-bdb2d2c51467', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+                 INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'd1428f06-d971-497d-ba0c-541b251a7046', N'guest@vidly.com', 0, N'AGyZDefXpoaVlUx190xgKHbjcC9Cdi3EbFNryRu4ZcrPejaihoY0SsTCnyTF0MVTlA==', N'61fdede2-5f66-40bf-87a4-5ebb9b450f32', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+
+                
+                  INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'23a69913-aeff-488c-b232-9e35ea3824b9', N'CanManageMovie')
+                    
+                  INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'a80b66dd-4248-46fe-a3c9-c9504b3f03c0', N'23a69913-aeff-488c-b232-9e35ea3824b9')
+
+        
+                ");
+        
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
